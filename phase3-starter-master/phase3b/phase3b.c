9766893745c4d693f6f9624fd9191a144e798fd2
hackerrank <me@hackerrank.com>
2021-04-22 00:20:40 -0700

    added init and handler

--- phase3-starter-master/phase3b/phase3b.c
+++ phase3-starter-master/phase3b/phase3b.c
@@ -21,9 +21,21 @@ int debugging3 = 1;
 int debugging3 = 0;
 #endif
 
-typedef struct Frame{
+typedef struct FreeFrames{
+    int frameNum;
+    FreeFrame *next;
+} FreeFrames;
 
-} Frame;
+typedef struct UsedFrames{
+    int frameNum;
+    int pageNum;
+    UsedFrames *next;
+} UsedFrames;
+
+FreeFrames *FreeFramesHead;
+UsedFrames *UsedFramesHead;
+
+int numPages;
 
 void debug3(char *fmt, ...)
 {
@@ -52,14 +64,36 @@ int
 P3FrameInit(int pages, int frames)
 {
     int result = P1_SUCCESS;
+    int i;
+    FreeFrames *cur;
 
     // check kernel mode
+    // TODO: forgot how to test this
 
     // initialize the frame data structures, e.g. the pool of free frames
-    Frame frame = malloc(sizeof(frame));
+    // initialize head of the free frames
+    FreeFramesHead = (FreeFrames *) malloc(sizeof(FreeFrames))
+    // set frame num to -1 for head node
+    FreeFramesHead->frameNum = -1;
+    FreeFramesHead->next = NULL;
+    cur = FreeFramesHead;
+    // create frames number of frames and insert them into the list Ascending order (1,2,3,..)
+    for(i = 0; i < frames; i++){
+        cur->next = (FreeFrames *) malloc(sizeof(FreeFrames));
+        cur->next->frameNum = i;
+        cur = cur->next;
+    }
+    //initialize head of the used frames
+    UsedFramesHead = (UsedFrames *) malloc(sizeof(UsedFrames))
+    UsedFramesHead->frameNum = -1;
+    UsedFramesHead->pageNum = -1;
+    UsedFramesHead->next = NULL;
     
-    
+    // set numPages
+    numPages = pages;
+
     // set P3_vmStats.freeFrames
+    P3_vmStats.freeFrames = frames;
 
     return result;
 }
@@ -121,7 +155,32 @@ P3PageFaultResolve(int pid, int page, int *frame)
         fill frame with zeros
     return P1_SUCCESS
     *******************/
-   
-    return P3_NOT_IMPLEMENTED;
+    int rc;
+    FreeFrames *freeTemp;
+    USedFrames *usedTemp;
+    if(FreeFramesHead->next != NULL){
+        // sets return to frame num
+        *frame = FreeFramesHead->next->frameNum;
+        // reduce free frames
+        P3_vmStats.freeFrames--;
+        // allocate space for used frame;
+        tempUsed = (UsedFrames *) malloc(sizeof(UsedFrames));
+        // add temp used to used frames
+        tempUsed->next = UsedFramesHead->next;
+        tempUsed->pageNum = page;
+        tempUsed->frameNum = *frame;
+        UsedFramesHead->next = tempUsed;
+    }
+    else{
+        rc = P3SwapOut(frame);
+        if(rc == P3_OUT_OF_SWAP){
+            return rc;
+        }
+    }
+    rc = P3SwapIn(pid, page, frame);
+    if(rc == P3_PAGE_NOT_FOUND){
+        // TODO: I dont get this line of psuedo code
+    }
+    return P1_SUCCESS;
 }
 
